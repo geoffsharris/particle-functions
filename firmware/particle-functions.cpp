77e7cab8384bc06aa63a1e64c13d6e70ecae996b
geoffsharris <geoffsharris@yahoo.com>
2015-08-23 20:29:36 -0700

    Update particle-functions.cpp

--- firmware/particle-functions.cpp
+++ firmware/particle-functions.cpp
@@ -1,49 +1,20 @@
-# include "particle-functions.h"
-# include <application.h>
+#include "particle-functions.h"
 
-extern WeatherData weather;
+int DST_offset = -7;
 
-void WeatherData :: message(int data)     // defined outside class definition
+void particleInit()
 {
- Serial.print("Timestamp: "); Serial.println(_weatherTime);
- Serial.print("Temp (F): "); Serial.println(_greenhouseTemp);
- Serial.print("Humidity (%): "); Serial.println(_greenhouseHumidity);
+    Particle.function ("ghData" , ghData);
+    Particle.function ("passProgram" , passProgramParam);
+    Particle.function ("motor" , motor);
+    Particle.function ("dst" , setDST);
+    setTimezone();
 }
 
-void WeatherData :: weatherData()
+int ghData(String data)
 {
-    _weatherTime = 0;
- _greenhouseTemp = 0;
- _greenhouseHumidity = 0;
- _backupGreenhouseTemp = 0;
- _backupGreenhouseHumidity = 0;
- _outsideTemp = 0;
- _outsideHumidity = 0;
-  _solar = 0;
- _high = 0;
- _low = 0;
-}
 
-void WeatherData :: weatherData(unsigned int weatherTime, int greenhouseTemp,int greenhouseHumidity,
-                                int backupGreenhouseTemp, int backupGreenhouseHumidity,int outsideTemp,
-                                int outsideHumidity, int solar, int high, int low)
- {
- _weatherTime = weatherTime;
- _greenhouseTemp = greenhouseTemp;
- _greenhouseHumidity = greenhouseHumidity;
- _backupGreenhouseTemp = backupGreenhouseTemp;
- _backupGreenhouseHumidity = backupGreenhouseHumidity;
- _outsideTemp = outsideTemp;
- _outsideHumidity = outsideHumidity;
-  _solar = solar;
- _high = high;
- _low = low;
- }
-
-
-int greenhouseData(String data)
-    {
-
+  
 
     //Expected parameters in CSV format
     // 1. Unix time stamp
@@ -56,7 +27,7 @@ int greenhouseData(String data)
     // 8. solar radiation
     // 9. forecast high
     // 10. forecast low
-
+    
 
     char copyStr[64];
     data.toCharArray(copyStr,64);
@@ -82,71 +53,62 @@ int greenhouseData(String data)
     p = strtok(NULL,",");
     int low = atoi(p);
 
-    weather.weatherData(weatherTime, greenhouseTemp, greenhouseHumidity, backupGreenhouseTemp,
+    /*weather.weatherData(weatherTime, greenhouseTemp, greenhouseHumidity, backupGreenhouseTemp,
                         backupGreenhouseHumidity, outsideTemp, outsideHumidity, solar, high, low);
+                        */
+                        
+                        
+    Serial.println("data loaded to greenhouseData");
+    Serial.println(data);
 
 return 1;
 }
-
-int setSeason(String command) // pass CSV AMsetback, PMsetback, dayProgram, nightProgram
+int passProgramParam(String data)
 {
    char copyStr[64];
-    command.toCharArray(copyStr,64);
+    data.toCharArray(copyStr,64);
     char *p = strtok(copyStr, ",");
 
-    AMsetback = atoi(p);
-    String AMsetback_string = p;
+    int AMsetback = atoi(p);
     p = strtok(NULL,",");
-    String PMsetback_string = p;
-    PMsetback = atoi(p);
+    int PMsetback = atoi(p);
     p = strtok(NULL,",");
-    dayProgram = atoi(p);
+    int dayProgram = atoi(p);
     p = strtok(NULL,",");
-    nightProgram = atoi(p);
-
-    Alarm.free(0);
-    Alarm.free(1);
-    Alarm.alarmRepeat(AMsetback,0,0, MorningAlarm);
-    Alarm.alarmRepeat(PMsetback,0,0, EveningAlarm);
-    Spark.publish("AMsetback", AMsetback_string);
-    Spark.publish("PMsetback", PMsetback_string);
+    int nightProgram = atoi(p);
+    
+    Serial.println("data loaded to setProgram");
+    Serial.println(data);
 
 return 1;
 }
 
-void particleInit()
+int motor(String data)
 {
- // time function setup
-  Time.zone(tzOffset);
-  Alarm.alarmRepeat(AMsetback,0,0, MorningAlarm);
-  Alarm.alarmRepeat(PMsetback,0,0, EveningAlarm);
-  Alarm.alarmRepeat(0,0,0, synchTime);
-
- // setup particle functions
- Spark.function("ghData", greenhouseData);
- Spark.function("test_call",call_function);
- Spark.function("setSeason", setSeason);
+    Serial.println("motor function called");
+    char copyStr[64];
+    data.toCharArray(copyStr,64);
+    int pass_param = atoi(copyStr); // converts character array into int, subtracts 1 to get index # of program row from programParam array
+    //char * param = new char[5]; // creates character array to store program variable from RESTful call
+    //strcpy(param, data.c_str()); // copies program variable into character array
+    //int pass_param = atoi(param); // converts character array into int, subtracts 1 to get index # of program row from programParam array
+        
+    //motor.setM1Speed(pass_param);
+    Serial.println("data loaded to Test Function");
+    Serial.println(data);
+    return 1;
 }
 
-// variables used in program
-int tzOffset = -7;
-int AMsetback = 7;
-int PMsetback = 22;
-int dayProgram = 1;
-int nightProgram = 3;
-
-// functions called by time functions
-void MorningAlarm();
+int setDST(String data)
 {
-
-}
-void EveningAlarm();
-{
-
+    char copyStr[64];
+    data.toCharArray(copyStr,64);
+    DST_offset = atoi(copyStr);
+    Serial.println("dst function called");
+     Serial.println(DST_offset);
 }
 
-// called once per day at midnight to synch photon
-void synchTime();
+void setTimezone() // call to set timezone offset from UTC
 {
- Spark.syncTime();
+Time.zone(DST_offset);
 }
