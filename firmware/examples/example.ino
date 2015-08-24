b87b83527dfc64c1eb0024fcf46b36e0a09f783e
geoffsharris <geoffsharris@yahoo.com>
2015-08-23 20:28:30 -0700

    Update example.ino

--- firmware/examples/example.ino
+++ firmware/examples/example.ino
@@ -1,1 +1,12 @@
+#include <application.h>
 
+extern int DST_offset;
+
+
+void particleInit();
+void setTimezone();
+
+int ghData(String data);
+int passProgramParam(String data);
+int motor(String data);
+int setDST(String data);
