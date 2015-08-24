7bee0c7ddb1a1f77fd71836586b92fe6a07a08de
geoffsharris <geoffsharris@yahoo.com>
2015-08-23 20:43:08 -0700

    Update particle-functions.cpp

--- firmware/particle-functions.cpp
+++ firmware/particle-functions.cpp
@@ -1,14 +1,12 @@
 #include "particle-functions.h"
-
 int DST_offset = -7;
-
 void particleInit()
 {
     Particle.function ("ghData" , ghData);
     Particle.function ("passProgram" , passProgramParam);
     Particle.function ("motor" , motor);
     Particle.function ("dst" , setDST);
-    setTimezone();
+    setTimezone(DST_offset);
 }
 
 int ghData(String data)
@@ -104,11 +102,12 @@ int setDST(String data)
     char copyStr[64];
     data.toCharArray(copyStr,64);
     DST_offset = atoi(copyStr);
+    setTimezone(); // call to reste timezone
     Serial.println("dst function called");
      Serial.println(DST_offset);
 }
 
-void setTimezone() // call to set timezone offset from UTC
+void setTimezone(DST_offset) // call to set timezone offset from UTC
 {
 Time.zone(DST_offset);
 }
